d64bad1c6da26c72489c71941bf510314479aff0
Mohamed Benjbara <mbenjbar@c3r8p4.1337.ma>
2026-07-16 11:17:55 +0100

    ex00 doneee

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,20 +1,21 @@
 #include "BitcoinExchange.hpp"
 
-bool check_btc(std::string val, double &btc_val)
+bool check_btc(std::string &val, double &btc_val, std::string &line)
 {
     char *fin = NULL;
     btc_val = std::strtod(val.c_str(), &fin);
     if (btc_val > 1000)
-        return (false);
+        return (std::cerr << "Error: too large a number." << std::endl, false);
     if (btc_val < 0)
-        return (false);
+        return (std::cerr << "Error: not a positive number." << std::endl, false);
     if (btc_val != btc_val)
-        return (false);
-    if ((fin && *fin != '\0') || isspace(val[0]))
-        return (false);
+        return (std::cerr << "Error: bad input => " << line << std::endl, false);
+    if (*fin != '\0' || isspace(val[0]))
+        return (std::cerr << "Error: bad input => " << line << std::endl, false);
+    return (true);
 }
 
-bool check_date_format(std::string date)
+bool check_date_format(std::string &date)
 {
     if (date.size() != 10)
         return (false);
@@ -24,25 +25,26 @@ bool check_date_format(std::string date)
     {
         if (i == 4 || i == 7)
             continue;
-        return (false);
+        if (!std::isdigit(date[i]))
+            return (false);
     }
+    return (true);
 }
 
 bool check_date(std::string &date, std::string &line)
 {
-    size_t len = date.size();
-    int year = atoi(date.substr(0, len - 6).c_str());
-    int day = atoi(date.substr(len - 2, 2).c_str());
-    int month = atoi(date.substr(len - 5, 2).c_str());
+    int year  = atoi(date.substr(0, 4).c_str());
+    int month = atoi(date.substr(5, 2).c_str());
+    int day   = atoi(date.substr(8, 2).c_str());
 
-    if (date < "2009-01-03")
-	    return (std::cerr << "Error: btc did not exist in this date => " << line << std::endl, false);
+
+    if (year <= 0 || month > 12 || month < 1 || day < 1)
+        return (std::cerr << "Error: bad input => " << line << std::endl, false);
+
     bool leap_year = false;
     if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
         leap_year = true;
-
-    if (month > 12 || month < 1 || day < 1)
-        return (std::cerr << "Error: bad input => " << line << std::endl, false);
+    
     if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
     {
         if (day > 31)
@@ -58,6 +60,8 @@ bool check_date(std::string &date, std::string &line)
         if ((leap_year && day > 29) || (!leap_year && day > 28))
             return (std::cerr << "Error: bad input => " << line << std::endl, false);
     }
+    if (date < "2009-01-03")
+	    return (std::cerr << "Error: btc did not exist in this date => " << line << std::endl, false);
     line = date;
     return (true);
 }
@@ -74,14 +78,15 @@ bool process_line(std::string &line, double &btc_val)
         return (std::cerr << "Error: bad input => " << line << std::endl, false);
     date = line.substr(0, pos);
     val = line.substr(pos + 3);
-    if (check_btc(val, btc_val) == false)
-        return (std::cerr << "Error: bad input => " << line << std::endl, false);
+    if (check_btc(val, btc_val, line) == false)
+        return (false);
     
     if (check_date_format(date) == false)
         return (std::cerr << "Error: bad input => " << line << std::endl, false);
 
     if (check_date(date, line) == false)
         return (false);
+    return (true);
 }
 
 void output_print(std::map<std::string, float> &data ,std::string &date, double &btc_val)
@@ -92,7 +97,6 @@ void output_print(std::map<std::string, float> &data ,std::string &date, double 
         return ;
     }
     std::map<std::string, float>::iterator it = data.upper_bound(date);
-    it = data.upper_bound(date);
     if (it != data.begin())
         --it;
     std::cout << date << " => " << btc_val << " = " << btc_val * it->second << std::endl;
